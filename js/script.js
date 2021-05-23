e5cf3bc949838ca252f27df9f2be789de5021ebb
Your Name <you@example.com>
2021-05-23 21:12:26 +0700

    day 2 (about, explore, testimonial)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// function scroll to show box-shadow
 function funScrol() {
     document.getElementById("header").style.boxShadow = "rgb(0 0 0 / 20%) 0px 1px 8px";
+    if (window.scrollY === 0) {
+        document.getElementById("header").style.boxShadow = "none";
+    }
 }
+// Use SwiperJS for testimonial slide
+var swiper = new Swiper('.swiper-testimonial', {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+})
